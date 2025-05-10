4cbc9672a5eb6d3581aa09dee205ea0af9ca4c00
ADITYA JEKKULA <114218150+ADITYAJEKKULA@users.noreply.github.com>
2025-05-10 15:01:44 -0700

    Update docker_server.sh
    Final cleanup: use only gunicorn for production

--- docker_server.sh
+++ docker_server.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 gunicorn backend.wsgi:application --bind 0.0.0.0:10000
 python manage.py runserver --settings=backend.settings 0.0.0.0:80
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Final cleanup: use only gunicorn for production
